f589e0ef2e83abb36a142739c87cf6874d1eabbc
Paulo Raimundo <paulo.raimundo@mycapital.com.br>
2022-10-07 17:46:15 -0300

    first commit

---
+++ src/main/java/com/school/SystemSchoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemSchoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemSchoolBackendApplication.class, args);
+	}
+
+}
